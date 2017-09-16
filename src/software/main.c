32e294ae9b0a73fd18f89fca5c9459c18dbb3f85
Bili Xu <xubili93@gmail.com>
2017-09-16 16:57:47 +0000

    Switch to PCIe library implementation

--- src/software/main.c
+++ src/software/main.c
@@ -1,76 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <fpga_pci.h>
+#include <fpga_mgmt.h>
+#include <utils/lcd.h>
 
 #define IN_BUF_SIZE 16
 #define OUT_BUF_SIZE 32
 
 // Memory address offsets
-#define INPUT_1_OFFSET 0x00010000
-#define INPUT_2_OFFSET 0x00001000
-#define OUTPUT_OFFSET 0x00002000
+#define INPUT_1_OFFSET UINT64_C(0x00010000)
+#define INPUT_2_OFFSET UINT64_C(0x00001000)
+#define OUTPUT_OFFSET UINT64_C(0x00002000)
+
+static uint16_t pci_vendor_id = 0x1D0F; /* Amazon PCI Vendor ID */
+static uint16_t pci_device_id = 0xF000; /* PCI Device ID preassigned by Amazon for F1 applications */
+
+int check_afi_ready(int slot_id);
+int multiply(int a, int b, uint32_t *result);
 
 int main(int argc, char *argv[])
 {
-    char *queue;
-    char *p;
-    int fd;
-    int ret;
+    int rc;
 
-    short* in1;
-    short* in2;
-    int* out;
+    rc = fpga_pci_init();
+    fail_on(rc, out, "Unable to initialize the fpga_pci library");
 
-    queue = argv[1];
+    rc = check_afi_ready(0);
+    fail_on(rc, out, "AFI not ready");
 
-    in1 = malloc(sizeof(short));
-    in2 = malloc(sizeof(short));
-    out = malloc(sizeof(int));
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    uint32_t result;
 
-    *in1 = (short) strtol(argv[2], &p, 10);
-    *in2 = (short) strtol(argv[3], &p, 10);
+    printf("Expecting: %d * %d = %d\n", a, b, a*b);
+    rc = multiply(a, b, &result);
+    fail_on(rc, out, "Multiplication failed");
+    printf("Actual: %d\n", result);
 
-    printf("On queue: %s\n", queue);
-    printf("Evaluating %d * %d with hardware multiplier.\n", *in1, *in2);
+    return rc;
 
-    // Open an EDMA queue for read/write
-    fd = open(queue, O_RDWR);
-    if (fd == -1) {
-        perror("open failed with errno");
+out:
+    return 1;
+}
+
+int multiply(int a, int b, uint32_t *result)
+{
+    int rc;
+    pci_bar_handle_t pci_bar_handle = PCI_BAR_HANDLE_INIT;
+
+    rc = fpga_pci_attach(0, FPGA_APP_PF, APP_PF_BAR4, 0, &pci_bar_handle);
+    fail_on(rc, out, "Unable to attach to the AFI on slot id %d", 0);
+
+    rc = fpga_pci_poke(pci_bar_handle, INPUT_1_OFFSET, a);
+    fail_on(rc, out, "Unable to write to FPGA");
+    rc = fpga_pci_poke(pci_bar_handle, INPUT_2_OFFSET, b);
+    fail_on(rc, out, "Unable to write to FPGA");
+
+    rc = fpga_pci_peek(pci_bar_handle, OUTPUT_OFFSET, result);
+    fail_on(rc, out, "Unalbe to read from the FPGA");
+
+    return rc;
+
+out:
+    if (pci_bar_handle >= 0) {
+        rc = fpga_pci_detach(pci_bar_handle);
+        if (rc) {
+            printf("Failure while detaching from the FPGA.\n");
+        }
     }
 
-    // Writes inputs to their respective offsets
-    ret = pwrite(fd, in1, IN_BUF_SIZE, INPUT_1_OFFSET);
-    if (ret < 0) {
-        perror("write failed with errno");
+    return (rc != 0 ? 1 : 0);
+}
+
+
+int check_afi_ready(int slot_id) {
+    struct fpga_mgmt_image_info info = {0};
+    int rc;
+
+    /* get local image description, contains status, vendor id, and device id. */
+    rc = fpga_mgmt_describe_local_image(slot_id, &info,0);
+    fail_on(rc, out, "Unable to get AFI information from slot %d. Are you running as root?",slot_id);
+
+    /* check to see if the slot is ready */
+    if (info.status != FPGA_STATUS_LOADED) {
+        rc = 1;
+        fail_on(rc, out, "AFI in Slot %d is not in READY state !", slot_id);
     }
 
-    ret = pwrite(fd, in2, IN_BUF_SIZE, INPUT_2_OFFSET);
-    if (ret < 0) {
-        perror("write failed with errno");
+    printf("AFI PCI  Vendor ID: 0x%x, Device ID 0x%x\n",
+        info.spec.map[FPGA_APP_PF].vendor_id,
+        info.spec.map[FPGA_APP_PF].device_id);
+
+    /* confirm that the AFI that we expect is in fact loaded */
+    if (info.spec.map[FPGA_APP_PF].vendor_id != pci_vendor_id ||
+        info.spec.map[FPGA_APP_PF].device_id != pci_device_id) {
+        printf("AFI does not show expected PCI vendor id and device ID. If the AFI "
+               "was just loaded, it might need a rescan. Rescanning now.\n");
+
+        rc = fpga_pci_rescan_slot_app_pfs(slot_id);
+        fail_on(rc, out, "Unable to update PF for slot %d",slot_id);
+        /* get local image description, contains status, vendor id, and device id. */
+        rc = fpga_mgmt_describe_local_image(slot_id, &info,0);
+        fail_on(rc, out, "Unable to get AFI information from slot %d",slot_id);
+
+        printf("AFI PCI  Vendor ID: 0x%x, Device ID 0x%x\n",
+            info.spec.map[FPGA_APP_PF].vendor_id,
+            info.spec.map[FPGA_APP_PF].device_id);
+
+        /* confirm that the AFI that we expect is in fact loaded after rescan */
+        if (info.spec.map[FPGA_APP_PF].vendor_id != pci_vendor_id ||
+             info.spec.map[FPGA_APP_PF].device_id != pci_device_id) {
+            rc = 1;
+            fail_on(rc, out, "The PCI vendor id and device of the loaded AFI are not "
+                             "the expected values.");
+        }
     }
 
-    // Ensure the write made it to the CL
-    fsync(fd);
+    return rc;
 
-    // Read output from the address offset
-    ret = pread(fd, out, OUT_BUF_SIZE, OUTPUT_OFFSET);
-    if (ret < 0) {
-        perror("read failed with errno");
-    }
-
-    if (close(fd) < 0) {
-        perror("close failed with errno");
-    }
-
-    printf("Expected result is %d\n", (*in1) * (*in2));
-    printf("Data read is %d\n", *out);
-
-    free(in1);
-    free(in2);
-    free(out);
-
-    return 0;
+out:
+    return 1;
 }
